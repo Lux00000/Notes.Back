8c1777a3aa7ba924bc9e8002c2bb69a7a6e3051f
Goudini <luxmain000@gmai.com>
2023-11-27 09:05:39 +0300

    update Notes.Persistance

---
+++ Notes.Persistance/NotesDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
+using NotesApplication.interfaces;
+using Notes.Persistance.EntityTypeConfigurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Persistance
+{
+    public class NotesDbContext : DbContext, INotesDbContext
+    {
+        public DbSet <Note> Notes { get; set;}
+        public NotesDbContext(DbContextOptions<NotesDbContext> options)
+            : base(options) { }
+        protected override void OnModelCreatting(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new NoteConfigurations());
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ NotesApplication/Interfaces/INotesDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
+
+namespace NotesApplication.interfaces
+{
+    public interface INotesDbContext
+    {
+        DbSet<Note> Notes { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
